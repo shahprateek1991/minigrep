792e3f3ad7249d0208a135413960fa74d8eb862f
Prateek Shah <shah.prateek1991@gmail.com>
2020-12-24 17:52:00 +0530

    Use struct for config

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,25 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let (query, filename) = parse_config(&args);
+    let config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let content = fs::read_to_string(filename)
+    let content = fs::read_to_string(config.filename)
         .expect("Could not read the file");
 
     println!("Content \n{}", content);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
 
-    (query, filename)
+    Config { query, filename }
+}
+
+struct Config {
+    query: String,
+    filename: String
 }
