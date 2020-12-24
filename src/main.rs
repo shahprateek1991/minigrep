93ce7ace9d286865c7892a8f1dfc00843a9c9dd3
Prateek Shah <shah.prateek1991@gmail.com>
2020-12-24 17:53:47 +0530

    Use struct constructor for parsing the files

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
@@ -14,14 +14,16 @@ fn main() {
     println!("Content \n{}", content);
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-
-    Config { query, filename }
-}
-
 struct Config {
     query: String,
     filename: String
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Config { query, filename }
+    }
 }
